ca06010a41aea857b67ff3e48d0e7845c56bc4d7
Nikita Maryniuk <nikitamaryniuk@gmail.com>
2024-10-07 21:53:33 +0200

    tailwind might be fixed now

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4e53107fc96cf448"; }
+/******/ 		__webpack_require__.h = function() { return "03b0dc587e908d18"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
